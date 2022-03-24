04c9c09a0b486bb85a88371d021e7ff3a6ee08c4
jorgebarrado <j.barrado.2016@alumnos.urjc.es>
2022-03-24 15:04:13 +0100

    home

---
+++ P1/tienda.js
@@ -0,0 +1,9 @@
+// -- PRACTICA 1: APLICACION WEB (TIENDA ONLINE)
+
+//-- Modulos
+const http = require('http');
+const fs = require('fs');
+
+//-- Pueto donde tiene que escuchar el servidor 
+const PUERTO = 9090;
+
